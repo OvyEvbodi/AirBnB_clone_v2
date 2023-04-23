3606f3077699b5515375c8ba162b219e44d37610
Ovy Evbodi <evbodiovo@gmail.com>
2023-04-23 18:07:09 +0100

    Add c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""This modiule starts a Flask web application,
+    that returns text based on the route
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Renders ``hello HBNB`` for the root route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Renders ``HBNB`` for the ``hbnb`` route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_path(text):
+    """Renders `C` followed by the text passed in the route"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
